227d1db65d2066f7d55b4fabd0dec4721ba9dee9
Kristen Chellis <peanutster@gmail.com>
2018-05-04 15:05:19 -0700

    add more Art objects to array

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -8,15 +8,18 @@ import {Art} from './models/art.model';
 
 export class SearchPipe implements PipeTransform {
   transform(input: Art[], search: string) {
-    let results: Art[];
-    for(var i = 0; i< input.length; i++){
-      for(var j = 0; j< input[i].keywords.length; j++){
-        let keyword =input[i].keywords[j].toLowerCase();
-        if(keyword.includes(search.toLowerCase())){
-          results.push(input[i]);
+    let results: Art[]= [];
+    if(search) {
+      for(var i = 0; i< input.length; i++){
+        for(var j = 0; j< input[i].keywords.length; j++){
+          let keyword =input[i].keywords[j].toLowerCase();
+          if(keyword.includes(search.toLowerCase())){
+            results.push(input[i]);
+          }
         }
       }
+      return results;
     }
-    return results;
+    return input;
   }
 }
